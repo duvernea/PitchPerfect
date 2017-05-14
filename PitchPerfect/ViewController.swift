9242c65addb61f985db29451751dca324af751cd
Brian DuVerneay <duverneay@gmail.com>
2017-05-14 10:43:55 -0500

    feat: Add image assets, update record buttons control

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -12,9 +12,18 @@ class ViewController: UIViewController {
 
     @IBOutlet var recordingLabel: UILabel!
     
+    @IBOutlet var recordButton: UIButton!
+    @IBOutlet var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear called")
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,12 +32,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func recordAudio(_ sender: Any) {
-        print("record button pressed")
         recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
 
     @IBAction func stopRecording(_ sender: Any) {
         print("stop recording button pressed")
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
     }
 }
 
