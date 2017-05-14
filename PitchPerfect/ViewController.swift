5fe64991e4da4346df85f89f7d05a586f86cd8bd
Brian DuVerneay <duverneay@gmail.com>
2017-05-14 09:27:33 -0500

    first commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by BRIAN DUVERNEAY on 5/13/17.
+//  Copyright © 2017 BRIAN DUVERNEAY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button pressed")
+    }
+
+}
+
