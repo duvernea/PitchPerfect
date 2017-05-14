c69cc24d9342f5b8d4226cdcc8c7eae4f99934b1
Brian DuVerneay <duverneay@gmail.com>
2017-05-14 09:52:53 -0500

    feat: Add label and 'stop recording' button

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,7 +24,11 @@ class ViewController: UIViewController {
 
     @IBAction func recordAudio(_ sender: Any) {
         print("record button pressed")
+        recordingLabel.text = "Recording in Progress"
     }
 
+    @IBAction func stopRecording(_ sender: Any) {
+        print("stop recording button pressed")
+    }
 }
 
